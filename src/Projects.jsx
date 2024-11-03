85f52413515c4a8ffa43e91504003bbbfc58439b
Akshay Vakil <117962555+Octrainn@users.noreply.github.com>
2024-11-03 12:59:03 -0500

    Added space simulator project

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -7,6 +7,7 @@ import calendar from './Images/Calendar.png';
 import tensor from './Images/TensorFlow.jpg';
 import discord from './Images/DiscordBot.png';
 import picture from './Images/Better.jpg';
+import SpaceSim from './Images/SpaceSim.png';
 
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -40,13 +41,13 @@ function Projects() {
         My Projects <span style={{ color: '#FF004F' }}>.</span>{' '}
       </h1>
       <div className='projects-wrapper'>
-        <ProjectCard
-          title="Health Policy Student Association"
-          imageSrc={medical}
-          githubLink="https://github.com/ChrisMikhail/hpsa-website"
-          technologies="HTML CSS JavaScript JQuery"
-          description="The website is a nonprofit organization aiming to raise awareness among students by providing information on corporate health and policies. It serves as a project proposal with a fully responsive design for both mobile and desktop platforms. Website url/link is posted in the READ ME."
-        />
+      <ProjectCard
+          title="Space Simualtor X"
+          imageSrc={SpaceSim}
+          githubLink="https://www.roblox.com/games/15775582193/UPD-4-Space-Simulator-X"
+          technologies="LUA, Roblox Game Engine, ProfileService API, TopBarPlus API"
+          description="Developed a fully functional game in Roblox, utilinz multiple tech stacks and APIs. Game is publically available, click the icon above."
+          />
 
         <ProjectCard
           title="Hackathon Fault Finder"
@@ -56,36 +57,15 @@ function Projects() {
           description="The project employs OpenAI's API to generate interview-level questions paired with intentionally incorrect solutions. It utilizes a backend database in Node.js to present users with these questions and solutions, allowing them to review and debug the provided code. The system then provides feedback on the accuracy of the debugging process."
         />
 
-        <ProjectCard
-          title="React Calendar"
-          imageSrc={calendar}
-          githubLink="https://github.com/Octrainn/React-Calendar-App"
-          technologies="React JSX, GH-Pages, Vite, CSS"
-          description="React-Calendar is a feature-rich calendar component built using React. Leveraging the power of React, it offers several dynamic and interactive features to enhance the user experience."
+  <ProjectCard
+          title="Health Policy Student Association"
+          imageSrc={medical}
+          githubLink="https://github.com/ChrisMikhail/hpsa-website"
+          technologies="HTML CSS JavaScript JQuery"
+          description="The website is a nonprofit organization aiming to raise awareness among students by providing information on corporate health and policies. It serves as a project proposal with a fully responsive design for both mobile and desktop platforms. Website url/link is posted in the READ ME."
         />
 
-        <ProjectCard
-          title="Discord Bot Weaver"
-          imageSrc={discord}
-          githubLink="https://github.com/Octrainn/DiscordBot"
-          technologies="Python, OAuth 2.0, Spotify API"
-          description="Versatile Discord bot that utilizes the Spotify API to stream music, and it offers several other functionalities like moderation, chat restriction, image generation, and meme generation."
-        />
 
-        <ProjectCard
-          title="Tensor Flow Model"
-          imageSrc={tensor}
-          githubLink="https://github.com/Octrainn/TensorFlowModel"
-          technologies="Python, Juypter Notebook, Google Colab"
-          description="Mini-project that focuses on implementing a machine learning model to accurately differentiate between images of humans and animals, specifically distinguishing between dogs and cats. Additionally, the project leverages the computational capabilities of Google Colab to efficiently train the model and handle the image datasets effectively."
-        />
-        <ProjectCard
-          title="Static Portfolio "
-          imageSrc={picture}
-          githubLink="https://github.com/Octrainn/octrainn.github.io"
-          technologies="HTML, CSS, JavaScript, JQuery, Bootstrap CSS"
-          description="Experience my previous static portfolio designed to elegantly display my abilities and projects. Its responsive and dynamic layout ensures seamless viewing across various devices, including laptops, mobiles, and PCs. Website url/link is posted in the READ ME."
-        />
       </div>
     </div>
   );
